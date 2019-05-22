362d3281ee8ca7741537fbac01802e059fca1056
ronneljamesdev <ronneljamesdev@pop-os.localdomain>
2019-05-22 22:27:11 +0800

    :bug: fix sidedrawer not closing

--- src/components/Navigation/ItemList/ItemList.js
+++ src/components/Navigation/ItemList/ItemList.js
@@ -1,18 +1,38 @@
+import PropTypes from "prop-types";
 import React from "react";
 import Item from "./Item/Item";
 import styles from "./ItemList.module.scss";
 
-const ItemList = () => {
+const ItemList = ({ authenticated }) => {
   return (
     <ul className={styles.ItemList}>
       <Item link="/" exact>
         Burger Builder
       </Item>
-      <Item link="/orders" exact>
-        Orders
-      </Item>
+      {authenticated ? (
+        <React.Fragment>
+          <Item link="/orders" exact>
+            Orders
+          </Item>
+          <Item link="/logout" exact>
+            Logout
+          </Item>
+        </React.Fragment>
+      ) : (
+        <Item link="/login" exact>
+          Login
+        </Item>
+      )}
     </ul>
   );
+};
+
+ItemList.defaultProps = {
+  authenticated: false
+};
+
+ItemList.propTypes = {
+  authenticated: PropTypes.bool.isRequired
 };
 
 export default ItemList;
